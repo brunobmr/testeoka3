eb753f2c7da49aba002ab1fa9c8995391fb1534c
brunobmr <110856334+brunobmr@users.noreply.github.com>
2023-09-28 22:43:04 -0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import LandingHome from "@/components/LandingSection/LandingHome";
+import OurWorkingSection from "@/components/HomePage/OurWorkingSection/OurWorkingSection";
+import SubpagesSection from "@/components/HomePage/SubPagesSection/SubpagesSection";
+import Footer from "@/components/Templates/Footer";
+import Head from "next/head";
+import styled from "styled-components";
+
+const IndexMain = styled.main`
+  background-color: var(--black-alt);
+  header,
+  section,
+  footer {
+    animation: delayanimation 1s forwards !important;
+  }
+`;
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/home-favicon.png" />
+        <title>OKA lev | Home Page</title>
+      </Head>
+      <IndexMain>
+        <LandingHome />
+        <SubpagesSection />
+        <OurWorkingSection />
+        <Footer />
+      </IndexMain>
+    </>
+  );
+}
